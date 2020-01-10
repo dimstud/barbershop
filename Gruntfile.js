7738077dc0a986f35dfc725a589f783a87ae7e7b
Dmitry Ageev <dimichageev@icloud.com>
2020-01-10 11:33:58 +0300

    Настроил grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,49 @@
-module.exports = function(grunt) {
-grunt.initConfig({
-less: {
-development: {
-options: {
-compress: true,
-yuicompress: true,
-optimization: 2
-},
+module.exports = function (grunt) {
+  require("load-grunt-tasks")(grunt);
 
-files: {
-"source/css/style.css": "source/less/style.less"
-}
-}
-},
-
-watch: {
-styles: {
-files: ['source/less/style.less'],
-tasks: ['less'],
-options: {
-nospawn: true
-}
-}
-}
-});
-
-grunt.loadNpmTasks('grunt-contrib-less');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.registerTask('default', ['watch']);
-
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "source/css/style.css": "source/less/style.less"
+        }
+      }
+    },
+    postcss: {
+      style: {
+        options: {
+          processors:[
+            require("autoprefixer")()
+          ]
+        },
+        src: "source/css/*.css"
+      }
+    },
+    watch: {
+      style: {
+        files: ["source/less/**/*/less"],
+        tasks: ["less", "postcss"]
+      }
+    },
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: ["source/*.html", "source/css/*.css"]
+        },
+        options: {
+          server: "source/"
+        }
+      }
+    },
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "source/css/style.min.css": ["source/css/style.css"]
+        }
+      }
+    }
+  });
 };
